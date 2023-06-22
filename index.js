936ee73d8b5192e2c4389f57f5a449d72e51f837
jephter-olamiposi <grandbusta@gmail.com>
2023-06-22 22:18:17 +0100

    cart functionality

--- index.js
+++ index.js
@@ -1,93 +1,174 @@
-let removeBtn = document.getElementsByClassName('remove-btn');
-let number = document.querySelectorAll(".quantity");
-let cartItemRemove = document.querySelectorAll('.cart-item')
-let up = document.getElementsByClassName("up");
-let down = document.getElementsByClassName("down");
-let totalAmount = document.querySelector('.total-amount')
+let items = [
+    {
+        image: 'src="https://www.course-api.com/images/cart/phone-1.png" alt="Samsung Galaxy S8"',
+        name: 'Samsung Galaxy S8',
+        price: '$399.99',
+        removebtn: 'remove',
+        buttonIncreament: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="amount-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"> <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path></svg>',
+        quantity: '1',
+        buttonDecreament: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="amount-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>'        
+    },
+    {
+        image: 'src="https://www.course-api.com/images/cart/phone-2.png" alt="google pixel"',
+        name: 'google pixel',
+        price: '$499.99',
+        removebtn: 'remove',
+        buttonIncreament: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="amount-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"> <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path></svg>',
+        quantity: '1',
+        buttonDecreament: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="amount-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>'        
+    },
+    {
+        image: ' src="https://www.course-api.com/images/cart/phone-3.png" alt="Xiaomi Redmi Note 2"',
+        name: 'Xiaomi Redmi Note 2',
+        price: '$699.99',
+        removebtn: 'remove',
+        buttonIncreament: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="amount-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"> <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path></svg>',
+        quantity: '1',
+        buttonDecreament: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="amount-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>'        
+    },
+    {
+        image: ' src="https://www.course-api.com/images/cart/phone-4.png" alt="Samsung Galaxy S7"',
+        name: 'Samsung Galaxy S7',
+        price: '$599.99',
+        removebtn: 'remove',
+        buttonIncreament: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="amount-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"> <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path></svg>',
+        quantity: '1',
+        buttonDecreament: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="amount-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>'        
+    }
+
+]
 
 
-//arrow up increament
-for (let i = 0; i < up.length; i++) {
-    let arrowUp = up[i]
-    arrowUp.addEventListener('click', () => {
-        let input = number[i]
-        let increase = Number(input.innerText) + 1
-        input.innerText = increase
-        updateCartTotal();
-    })
-
+for(let i=0;i<items.length; i++){
+    document.querySelector('.cart-container').innerHTML += `
+<div class="cart-item">
+<img ${items[i].image}>
+<div>
+    <h5>${items[i].name}</h5>
+     <span class="item-price">${items[i].price}</span>
+    <button class="remove-btn">${items[i].removebtn}</button>
+</div>
+<div>
+    <button class="amount-btn up" >${items[i].buttonIncreament}</button>
+    <div class="quantity">${items[i].quantity}</div>
+    <button class="amount-btn down" >${items[i].buttonDecreament}</button>
+</div>
+</div>`;
 }
-//arrow down decreament
-for (let i = 0; i < down.length; i++) {
-    let arrowDown = down[i]
-    arrowDown.addEventListener('click', () => {
 
-
-        let input = number[i]
-        let increase = Number(input.innerText) - 1
-        if (increase <= 0) {
-            cartItemRemove[i].remove()
-            console.log(cartItemRemove[i])
+let cartItems=document.querySelectorAll('.cart-item')
+for(let i=0; i<cartItems.length; i++){
+    let re=cartItems[i].querySelector('.remove-btn')
+    re.addEventListener('click',()=>{
+        let cartContainer = document.getElementsByClassName('cart-container')[0]
+        let cartItems = cartContainer.getElementsByClassName('cart-item')
+        let total = 0
+        let qty=cartItems[i].querySelector('.quantity')
+        let quantity=Number(qty.innerText) 
+        qty.innerText=quantity-1
+        for (let i = 0; i < cartItems.length; i++) {
+            let cartItem = cartItems[i]
+            let itemPrice = cartItem.getElementsByClassName('item-price')[0];
+            let itemQuantity = cartItem.getElementsByClassName('quantity')[0];
+            let price = parseFloat(itemPrice.innerText.replace('$', ''))
+            let quantity = Number(itemQuantity.innerText)
+            total = total + (price * quantity)
         }
-        input.innerText = increase
-        updateCartTotal()
+        
+        total = Math.round(total * 100) / 100
+        document.getElementsByClassName('total-price')[0].innerText = '$' + total
+        console.log(total)
+        let amount=document.querySelector('.total-amount')
+        let cartQty=Number(amount.innerText)
+        amount.innerText= cartQty- 1
+        if(amount.innerText<=0){
+            document.querySelector('.cart').innerHTML = `
+            <div class="go">
+                <h4>YOUR BAG</h4>
+                <p>is currently empty</p>
+            </div>`;
+        }
+       cartItems[i].remove()
 
+       
     })
-
 }
-// remove button
-hideitem()
-function hideitem() {
-    for (let i = 0; i < removeBtn.length; i++) {
-        let button = removeBtn[i]
-        button.addEventListener('click', removeCartItems)
-
-    }
-}
-
-function removeCartItems(event) {
-    let buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal();
-
-}
-
-// cart update
-function updateCartTotal() {
-    let cartContainer = document.getElementsByClassName('cart-container')[0]
-    let cartItems = cartContainer.getElementsByClassName('cart-item')
-    let total = 0
-    for (let i = 0; i < cartItems.length; i++) {
-        let cartItem = cartItems[i]
-        let itemPrice = cartItem.getElementsByClassName('item-price')[0];
-        let itemQuantity = cartItem.getElementsByClassName('quantity')[0];
-        let price = parseFloat(itemPrice.innerText.replace('$', ''))
-        let quantity = Number(itemQuantity.innerText)
-        console.log(price * quantity)
-
-        total = total + (price * quantity)
-    }
-    let change = cartItems.length
-    totalAmount.innerHTML = change
-    if (change == 0) {
-        empty()
-    }
-    total = Math.round(total * 100) / 100
-    document.getElementsByClassName('total-price')[0].innerText = '$' + total
-}
-// clear button
-let clear = document.querySelector('.clear-btn')
-let cart = document.querySelector('.cart')
-
+let clear=document.querySelector('.clear-btn')
 clear.addEventListener('click', () => {
-    empty()
-})
-
-function empty() {
-    document.querySelector('.show').innerHTML = `
+    document.querySelector('main').innerHTML = `
     <div class="go">
         <h4>YOUR BAG</h4>
         <p>is currently empty</p>
     </div>`;
-    cart.remove()
+    
+})
+
+for(let i=0; i<cartItems.length; i++){
+    let inc=cartItems[i].querySelector('.up')
+
+    // removeBtn
+    inc.addEventListener('click',()=>{
+        let cartContainer = document.getElementsByClassName('cart-container')[0]
+        let cartItems = cartContainer.getElementsByClassName('cart-item')
+        let total = 0
+        let qty=cartItems[i].querySelector('.quantity')
+        let quantity=Number(qty.innerText) 
+        qty.innerText=quantity+1
+        for (let i = 0; i < cartItems.length; i++) {
+            let cartItem = cartItems[i]
+            let itemPrice = cartItem.getElementsByClassName('item-price')[0];
+            let itemQuantity = cartItem.getElementsByClassName('quantity')[0];
+            let price = parseFloat(itemPrice.innerText.replace('$', ''))
+            let quantity = Number(itemQuantity.innerText)
+            total = total + (price * quantity)
+        }
+        
+        total = Math.round(total * 100) / 100
+        document.getElementsByClassName('total-price')[0].innerText = '$' + total
+        console.log(total)
+        let amount=document.querySelector('.total-amount')
+        let cartQty=Number(amount.innerText)
+        amount.innerText= cartQty+ 1
+
+    })
+    let dec=cartItems[i].querySelector('.down')
+
+    dec.addEventListener('click',()=>{
+        let cartContainer = document.getElementsByClassName('cart-container')[0]
+        let cartItems = cartContainer.getElementsByClassName('cart-item')
+        let total = 0
+        let qty=cartItems[i].querySelector('.quantity')
+        let quantity=Number(qty.innerText) 
+        qty.innerText=quantity-1
+        for (let i = 0; i < cartItems.length; i++) {
+            let cartItem = cartItems[i]
+            let itemPrice = cartItem.getElementsByClassName('item-price')[0];
+            let itemQuantity = cartItem.getElementsByClassName('quantity')[0];
+            let price = parseFloat(itemPrice.innerText.replace('$', ''))
+            let quantity = Number(itemQuantity.innerText)
+            total = total + (price * quantity)
+        }
+        
+        total = Math.round(total * 100) / 100
+        document.getElementsByClassName('total-price')[0].innerText = '$' + total
+        console.log(total)
+        let amount=document.querySelector('.total-amount')
+        let cartQty=Number(amount.innerText)
+        amount.innerText= cartQty- 1
+
+
+        if (qty.innerText<=0){
+            cartItems[i].remove()
+        }
+        if(amount.innerText<=0){
+            document.querySelector('.cart').innerHTML = `
+            <div class="go">
+                <h4>YOUR BAG</h4>
+                <p>is currently empty</p>
+            </div>`;
+        }
+
+    })
 }
+
+
